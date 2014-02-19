4ca536ca8cf93990f94d680cb354a5021347920c
themythicaldrago <ryan.louie@students.olin.edu>
2014-02-18 20:52:47 -0500

    Random Art is able to create random bitmap computational art pieces

--- hw4/random_art.py
+++ hw4/random_art.py
@@ -9,9 +9,10 @@ Created on Tue Feb 11 11:34:57 2014
 from random import randint
 import Image
 import math
+# import pygame
 
 base_fs = ["x", "y"]
-other_fs = ["prod", "avg", "cos_pi", "sin_pi", "sigmoid",]
+other_fs = ["prod", "avg", "cos_pi", "sin_pi",] #"sigmoid",]
 all_fs = base_fs + other_fs
 
 end_base = len(base_fs) - 1
@@ -81,16 +82,56 @@ def remap_interval(val, input_interval_start, input_interval_end, output_interva
     to the output interval [output_interval_start, output_interval_end].  The mapping
     is an affine one (i.e. output = input*c + b).
 
-    TODO: please fill out the rest of this docstring
+    inputs:
+        val: value, based on input domain
+        input_interval_start: min value of input domain
+        input_interval_end: max value of input domain
+        output_interval_start: min value of output domain
+        output_interval_end: max value of output domain
+
+    returns:
+        remapped value of input domain to output domain
     """ 
 
     percentile = float(val - input_interval_start) / (input_interval_end - input_interval_start) 
     return percentile*(output_interval_end - output_interval_start) + output_interval_start
 
 if __name__ == "__main__":
-    red = build_random_function(3,5)
-    green = build_random_function(3,5)
-    blue = build_random_function(3,5)
+    fred = build_random_function(10,15)
+    fgreen = build_random_function(10,15)
+    fblue = build_random_function(10,15)
+
+    print "red(x,y): \n", fred
+    print "green(x,y): \n", fgreen
+    print "blue(x,y): \n", fblue
+
+
+    def red(x,y): 
+        val = evaluate_random_function(fred, x, y)
+        return int(remap_interval(val, -1, 1, 0, 255))
+
+    def green(x,y): 
+        val = evaluate_random_function(fgreen, x, y)
+        return int(remap_interval(val, -1, 1, 0, 255))
+    
+    def blue(x,y): 
+        val = evaluate_random_function(fblue, x, y)
+        return int(remap_interval(val, -1, 1, 0, 255))
+
 
     im = Image.new("RGB", (350, 350))
-    
+    horz, vert = im.size
+
+    for i in xrange(horz):
+        x = remap_interval(i, 0, horz-1, -1, 1)
+        for j in xrange(vert):
+            y = remap_interval(j, 0, vert-1, -1, 1)
+            im.putpixel((i,j), (red(x,y), green(x,y), blue(x,y)))
+
+    im.save("image.bmp")
+
+    # pygame.init()
+    # size = width,height= (im.get_rect().width,im.get_rect().height)
+    # screen = pygame.display.set_mode(size)
+    # screen.blit(im,im.get_rect())
+    # pygame.display.flip()
