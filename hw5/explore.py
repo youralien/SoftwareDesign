28a21052ea74248bf02826a50c59078497331b75
Deniz Celik <dcelik@TurkishDelight.(none)>
2014-03-03 02:23:48 -0500

    added saturdays work

--- hw5/explore.py
+++ hw5/explore.py
@@ -1,10 +1,29 @@
 
 from pattern.web import *
+from pattern.en import *
+
 
 w = Wikipedia()
 
 def grabarticle(articlename):
+    #returns a list of strings corresponding to each section content
     article = w.search(articlename)
-    return article
+    artsec = article.sections
+    art = [i.string[len(i.title):] for i in artsec[1:]]
+    artfull = [artsec[0].string]
+    artfull.extend(art)
+    artstr = ' '.join(artfull)
+    return artstr
+    
+def grabarticlelinkless(articlename):
+    artstr = grabarticle(articlename)
+    artstr = artstr[:artstr.index(" * ^")]
+    print artstr
+    return artstr
 
-print grabarticle('Olin College').sections
+    
+g = grabarticlelinkless('Japanese American Internment')
+a=grabarticle('Japanese American Internment')
+#print a
+print sentiment(a)
+print sentiment(g)
